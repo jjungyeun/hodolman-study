79f51dc3b7eae193c3c7c852951c8a443dc7f9cb
jjungyeun <wjyddd@naver.com>
2023-06-16 17:44:54 +0900

    chore - Querydsl 관련 gradle 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+// for Querydsl
+buildscript {
+    dependencies {
+        classpath("gradle.plugin.com.ewerk.gradle.plugins:querydsl-plugin:1.0.10")
+    }
+}
+
 plugins {
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
     kotlin("plugin.jpa") version "1.8.21"
+    kotlin("kapt") version "1.7.21"
+    idea
 }
 
 group = "com.wonjung"
@@ -24,6 +33,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // for Querydsl
+    implementation("com.infobip:infobip-spring-data-jpa-querydsl-boot-starter:8.0.0")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 }
 
 tasks.withType<KotlinCompile> {
@@ -35,4 +48,13 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// for Querydsl
+idea {
+    module {
+        val kaptMain = file("$buildDir/generated/source/kapt/main")
+        sourceDirs.add(kaptMain)
+        generatedSourceDirs.add(kaptMain)
+    }
 }
