919e0e648d79aedfff28ef52ca1393f6d1758249
jjungyeun <wjyddd@naver.com>
2023-06-26 18:35:45 +0900

    feat - 글 상세 페이지 화면 만들기
    - 상세 페이지를 보여주는 DetailView 구현
    - HomeView에서 글 제목을 누르면 상세 페이지로 넘어가도록 구현

---
+++ front/src/views/DetailView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import axios from "axios";
+import {ref} from "vue";
+
+const props = defineProps({
+  postId: {
+    type: [Number, String],
+    require: true
+  }
+});
+
+const post = ref({
+  id: 0,
+  title: "",
+  content: ""
+});
+
+axios.get(`/be-api/posts/${props.postId}`)
+.then((response) => {
+  post.value = response.data;
+})
+
+</script>
+<template>
+  <h2>{{post.title}}</h2>
+  <div>{{post.content}}</div>
+</template>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
 import axios from "axios";
 import {ref} from "vue";
+import {RouterLink, useRouter} from "vue-router";
 
 const posts = ref([]);
+const router = useRouter();
 
-axios.get("/be-api/posts?page=1&size=5")
+axios.get("/be-api/posts")
     .then(response => {
       response.data.content.forEach((p: any) => {
         posts.value.push(p);
       })
-    })
+    });
+
+const moveToDetail = function () {
+  router.push({name: "detail"})
+}
 
 </script>
 
 <template>
   <ul>
-    <li v-for="(post, idx) in posts" :key="post.id">
-      <p class="mb-2">
-        {{posts.length - idx}} {{post.title}} {{post.content}}
-      </p>
+    <li v-for="(post, idx) in posts" :key="post.id" @click="moveToDetail()">
+      <div class="mb-2">
+        {{posts.length - idx}} <RouterLink :to="{name: 'detail', params: {postId: post.id}}">{{post.title}}</RouterLink>
+      </div>
     </li>
   </ul>
 </template>
